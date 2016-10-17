4a2a21ba59b4ae19c5c91c746057abcd3fcc3193
BertuccioDM <poletaev_dd@mail.ru>
2016-10-17 13:40:57 +0300

    Added index.html

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,15 +1,15 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
-@RestController
+@Controller
 @EnableAutoConfiguration
 public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return "index.html";
     }
 
     public static void main(String[] args) throws Exception {
