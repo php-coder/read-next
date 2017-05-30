a3a3fd0129950c4d18c8635fcf1cc159e83048ca
Slava Semushin <slava.semushin@gmail.com>
2017-05-30 19:23:16 +0200

    Import application skeleton generated by start.spring.io

---
+++ src/main/java/ru/mystamps/readnext/ReadnextApplication.java
@@ -0,0 +1,12 @@
+package ru.mystamps.readnext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadnextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadnextApplication.class, args);
+	}
+}
